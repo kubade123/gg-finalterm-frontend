f27b608a064844b33fa08c30bdb9da190c5cf815
Mufid <mibeyo123@gmail.com>
2023-08-17 11:12:53 +0700

    rever axios changes

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://gg-finalterm-backend.vercel.app",
+  baseURL: "http://127.0.0.1:3000",
 });
 
 export default instance;
